d2d3501889356f7754208801f4822c5279df661f
elleromero <elleromero@users.noreply.github.com>
2023-11-29 19:57:26 +0800

    added auth dao

--- models/User.cs
+++ models/User.cs
@@ -9,7 +9,7 @@ namespace libsys.models
     public class User
     {
 
-        public int ID { get; set; }
+        public Guid ID { get; set; }
         public Role Role { get; set; } = new Role();
         public Member Member { get; set; } = new Member();
         public string? Username { get; set; } = string.Empty;
